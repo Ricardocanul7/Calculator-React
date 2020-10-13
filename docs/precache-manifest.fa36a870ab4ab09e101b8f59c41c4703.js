a5ab49b3777e5688ddb7440330aa38119aba3cc1
Ricardo Canul <ricardocanul7@gmail.com>
2020-10-13 03:43:24 -0500

    Se genera build para github pages

---
+++ docs/precache-manifest.fa36a870ab4ab09e101b8f59c41c4703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb26000c7cb8baf486585c6cdf1b75a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "401ad50edc659bc01ea0",
+    "url": "/static/css/main.eba91b44.chunk.css"
+  },
+  {
+    "revision": "277def570aed580372d8",
+    "url": "/static/js/2.72dd93fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72dd93fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401ad50edc659bc01ea0",
+    "url": "/static/js/main.f20769bd.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
